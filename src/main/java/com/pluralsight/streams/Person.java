f5b25beda30339fa96c8336932df453eb3b5dc8a
Julian Palmgren <julianpalmgren@gmail.com>
2025-11-05 10:32:55 -0500

    Commit #1:
    * Worked on:
    - Program structure
    - Scanner input
    - For loop for finding first/last name matches

---
+++ src/main/java/com/pluralsight/streams/Person.java
@@ -0,0 +1,51 @@
+package com.pluralsight.streams;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // GETTERS
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // SETTERS
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
